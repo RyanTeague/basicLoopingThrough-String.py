60a8ed9b8b610879fe57ff54615b80b7ed5ebd2e
RyanTeague <Qews_3124>
2024-12-10 21:31:38 -0700

    first commit

---
+++ demo.py
@@ -0,0 +1,10 @@
+a = "bannanas"
+print(a[3])
+for a in "bannanas":
+    print(a)
+
+
+# checking if a character is present in string
+y = "charries"
+if "r" in y:
+    print("yes r is present in y")
